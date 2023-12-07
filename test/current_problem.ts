648565e7239ac737aa3af0b8a3df90a732a31ff9
Keith Braithwaite <keith.braithwaite@coop.co.uk>
2023-12-07 15:12:11 +0000

    clear decks for day 2

---
+++ test/current_problem.ts
@@ -0,0 +1,13 @@
+import {pipe} from 'fp-ts/function'
+import {reduce} from "fp-ts/Array";
+import {promises as filesystem} from "fs";
+
+
+
+describe("Advent of Code",()=> {
+    describe("2", () => {
+
+    })
+})
+
+export{}
