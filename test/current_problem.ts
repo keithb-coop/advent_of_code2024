283f6d87f296f51418a735d4643731e4c7cfa1db
Keith Braithwaite <keith.braithwaite@coop.co.uk>
2023-12-11 16:06:14 +0000

    Day 2 part 2 solved

--- test/current_problem.ts
+++ test/current_problem.ts
@@ -1,4 +1,4 @@
-import {filter, foldMap, reduce} from "fp-ts/Array";
+import {filter, foldMap, map, reduce} from "fp-ts/Array";
 import {promises as filesystem} from "fs";
 
 import {Monoid} from "fp-ts/Monoid";
@@ -11,14 +11,19 @@ const forAll: Monoid<boolean> = {
     empty: true
 }
 
+const sumAll: Monoid<number> = {
+    concat: (l: number, r:number) => l + r,
+    empty: 0
+}
+
 class Round {
 
     readonly _counts: Map<Colour, number>
 
-    constructor(counts:Array<[string, Colour]>) {
+    constructor(counts:Array<[number, Colour]>) {
         this._counts = new Map()
         for (const count of counts) {
-            this._counts.set(count[1] as Colour, parseInt(count[0]))
+            this._counts.set(count[1] as Colour, count[0])
         }
     }
 
@@ -34,6 +39,24 @@ class Round {
 
     couldProvide(candidateSet: Round) {
         return foldMap(forAll)(this.enoughOfFor(candidateSet))(Colours)
+    }
+
+    get power() {
+        let result = 1
+        for (const v of this._counts.values()) { //it would be nice to use map(), but values() is a Iterator not an array
+            result = result * v
+        }
+        return result
+    }
+
+    minimumRequiredWith(anotherRound: Round): Round {
+        const stuff: Array<[number, Colour]> = pipe(Colours, map((colour:Colour) => [this.biggerCount(colour, anotherRound), colour as Colour]))
+        return new Round(stuff)
+
+    }
+
+    private biggerCount(colour: Colour, anotherRound: Round):number {
+        return Math.max(this.numberOf(colour) ?? 0, anotherRound?.numberOf(colour) ?? 0);
     }
 }
 
@@ -53,6 +76,12 @@ class Game {
 
     couldBePlayedWity(ballSupply: Round) {
         return foldMap(forAll)((myGame: Round) => ballSupply.couldProvide(myGame))(this._rounds)
+    }
+
+    get minimumCubeSupply(): Round{
+        return pipe(this._rounds,
+            reduce(new Round([[0, Red], [0, Green], [0, Blue]]),
+                (accumulator: Round, next: Round) => accumulator.minimumRequiredWith(next)))
     }
 }
 
@@ -99,7 +128,7 @@ function createParser() {
         }
     }).addOperation('parseCubesData', {
         Cubes(count, colour) {
-            return [count.parseNumberData(), colour.sourceString]
+            return [parseInt(count.parseNumberData()), colour.sourceString]
         }
     }).addOperation('parseNumberData', {
         number(digits): string {
@@ -109,45 +138,99 @@ function createParser() {
     return {grammar, gameSemantics};
 }
 
+function sumIdsOfPossibleGames(games: Game[], cubeSupply: Round) {
+    return pipe(games,
+        filter((aGame: Game) => aGame.couldBePlayedWity(cubeSupply)),
+        reduce(0, (accumulator: number, aGame: Game) => accumulator + aGame.gameNumber));
+}
+
+function makeGames(rawData: string) {
+    const {grammar, gameSemantics} = createParser();
+    return gameSemantics(grammar.match(rawData)).parseGamesData();
+}
+
+function findPowerSumMinimumSets(games: Game[]) {
+    return foldMap(sumAll)((g: Game) => g.minimumCubeSupply.power)(games);
+}
+
 describe("Advent of Code",()=> {
     describe("using ohm", () => {
-        it("builds games",()=> {
-            const {grammar, gameSemantics} = createParser();
+        describe("Day 2",()=> {
+            describe("Part 1", () => {
+                it("builds games", () => {
 
-            const exmapleGames = "Game 17: 3 red, 2 green, 4 blue; 2 red, 4 green, 17 blue \n Game 42: 2 green, 6 blue"
-            const matchResult = grammar.match(exmapleGames)
-            expect(matchResult.succeeded()).toBeTruthy()
+                    const {grammar, gameSemantics} = createParser();
+                    const exmapleGames = "Game 17: 3 red, 2 green, 4 blue; 2 red, 4 green, 17 blue \n Game 42: 2 green, 6 blue"
+                    expect(grammar.match(exmapleGames).succeeded()).toBeTruthy()
 
-            const games: Game[] = gameSemantics(matchResult).parseGamesData()
-            expect(games.length).toEqual(2)
-            expect(games[0].gameNumber).toEqual(17)
+                    const games: Game[] = gameSemantics(grammar.match(exmapleGames)).parseGamesData()
+                    expect(games.length).toEqual(2)
+                    expect(games[0].gameNumber).toEqual(17)
+                })
+
+                it("recognises validity", () => {
+                    const {grammar, gameSemantics} = createParser();
+
+                    const exmapleGames = "Game 17: 3 red, 2 green, 4 blue; 2 red, 4 green, 17 blue \n Game 42: 2 green, 6 blue"
+                    const games: Game[] = gameSemantics(grammar.match(exmapleGames)).parseGamesData()
+                    const cubeSupply = new Round([[12, Red], [13, Green], [14, Blue]])
+                    expect(games[0].couldBePlayedWity(cubeSupply)).toBeFalsy()
+                    expect(games[1].couldBePlayedWity(cubeSupply)).toBeTruthy()
+                })
+
+                it("finds the answer", async () => {
+                    const rawData = await filesystem.readFile('problem_sets/Day2.txt', 'utf-8')
+                    //console.log(grammar.trace(rawData))
+                    const games = makeGames(rawData)
+                    const cubeSupply = new Round([[12, Red], [13, Green], [14, Blue]])
+                    expect(sumIdsOfPossibleGames(games, cubeSupply)).toEqual(2447)
+                })
+            })
         })
 
+        describe("day 2",()=>{
+            it("Calculates powers",() => {
+                const aRound = new Round([[4, Red], [2, Green], [6, Blue]])
+                expect(aRound.power).toEqual(48)
+                const anotherRound = new Round([[1, Red], [3, Green], [4, Blue]])
+                expect(anotherRound.power).toEqual(12)
+            })
 
-        it("recognises validity",()=> {
-            const {grammar, gameSemantics} = createParser();
+            it("makes minimum rounds",()=>{
+                const aRound = new Round([[4, Red], [2, Green], [6, Blue]])
+                const anotherRound = new Round([[7, Red], [1, Green], [6, Blue]])
+                expect(aRound.minimumRequiredWith(anotherRound)).toEqual(new Round([[7, Red],[2, Green],[6, Blue]]))
+            })
 
-            const exmapleGames = "Game 17: 3 red, 2 green, 4 blue; 2 red, 4 green, 17 blue \n Game 42: 2 green, 6 blue"
-            const games: Game[] = gameSemantics(grammar.match(exmapleGames)).parseGamesData()
-            const cubeSupply = new Round([["12", Red], ["13", Green], ["14", Blue]])
-            expect(games[0].couldBePlayedWity(cubeSupply)).toBeFalsy()
-            expect(games[1].couldBePlayedWity(cubeSupply)).toBeTruthy()
-        })
+            it.each([
+                [makeGames("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"), 4, 2, 6],
+                [makeGames("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"), 1, 3, 4],
+                [makeGames("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"), 20, 13, 6],
+                [makeGames("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"), 14, 3, 15],
+                [makeGames("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), 6, 3, 2]])
+            ("finds minimum cube supply",(games, r,g,b) => {
+                expect(games[0].minimumCubeSupply).toEqual(new Round ([[r, Red], [g, Green], [b, Blue]]))
+            })
 
-        it("finds the answer", async () => {
-            const {grammar, gameSemantics} = createParser();
-            const rawData = await filesystem.readFile('problem_sets/Day2.txt', 'utf-8')
-            //console.log(grammar.trace(rawData))
-            const matchResult = grammar.match(rawData)
-            expect(matchResult.succeeded()).toBeTruthy()
-            const games = gameSemantics(matchResult).parseGamesData()
+            it("finds the answer",async ()=>{
+                const exampleGames = makeGames(String.raw`
+                    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                `)
 
-            const cubeSupply = new Round([["12", Red], ["13", Green], ["14", Blue]])
+                const result =
+                        findPowerSumMinimumSets(exampleGames)
 
-            const result = pipe(games,
-                filter((aGame: Game)=>aGame.couldBePlayedWity(cubeSupply)),
-                reduce(0, (accumulator: number, aGame: Game) => accumulator + aGame.gameNumber))
-            expect(result).toEqual(2447)
+                expect(result).toEqual(2286)
+
+                const rawData = await filesystem.readFile('problem_sets/Day2.txt', 'utf-8')
+                const games = makeGames(rawData)
+                expect(findPowerSumMinimumSets(games)).toEqual(56322)
+
+            })
         })
     })
 })
